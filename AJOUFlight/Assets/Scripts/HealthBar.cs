20aae66a534b70f2a86586a221461cfe4ee7560a
misubz <misukisu.mux@gmail.com>
2021-05-11 11:35:36 +0900

    HP bar

--- AJOUFlight/Assets/Scripts/HealthBar.cs
+++ AJOUFlight/Assets/Scripts/HealthBar.cs
@@ -9,15 +9,23 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     public Slider slider;
+    public Gradient gradient;
+    public Image fill;
 
     public void SetMaxHealth(int health)
     {
         slider.maxValue = health;
         slider.value = health;
+
+        // If we want to healthbar to change color from the green to red
+        fill.color = gradient.Evaluate(1f);
     }
 
     public void SetHealth(int health)
     {
         slider.value = health;
+
+        // If we want to healthbar to change color from the green to red
+        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 }
