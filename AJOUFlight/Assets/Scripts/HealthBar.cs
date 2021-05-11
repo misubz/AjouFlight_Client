4bba8ccc6b83a40a5cb106305abd8fa37fb27586
misubz <misukisu.mux@gmail.com>
2021-05-11 10:58:43 +0900

    HP bar

---
+++ AJOUFlight/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Link to the tutor video
+// https://www.youtube.com/watch?v=BLfNP4Sc_iA
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+    }
+}
