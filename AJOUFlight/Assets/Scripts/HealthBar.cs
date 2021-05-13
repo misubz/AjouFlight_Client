a16cc1a37a7f1be6246d0a95999b654ddf84877e
misubz <misukisu.mux@gmail.com>
2021-05-14 01:21:21 +0900

    Final HP

--- AJOUFlight/Assets/Scripts/HealthBar.cs
+++ AJOUFlight/Assets/Scripts/HealthBar.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-// Link to the tutor video
+// Sources
 // https://www.youtube.com/watch?v=BLfNP4Sc_iA
 
 public class HealthBar : MonoBehaviour
@@ -12,16 +12,33 @@ public class HealthBar : MonoBehaviour
     public Gradient gradient;
     public Image fill;
 
+    public float health;
+
+    // Begin the MaxHealth with 100.
+    void Start()
+    {
+        SetMaxHealth(100);
+    }
+
     public void SetMaxHealth(int health)
     {
         slider.maxValue = health;
         slider.value = health;
 
     }
-
+    
     public void SetHealth(int health)
     {
         slider.value = health;
 
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            health = health - 10f;
+            SetHealth(10); // set the health bar with changed health.
+        }
     }
 }
